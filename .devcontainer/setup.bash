e9928f6120f5c37da77f2a3e8fb25ce66deb96d5
Johannes Köster <johannes.koester@tu-dortmund.de>
2025-07-09 16:40:33 +0200

    Update setup.bash

--- .devcontainer/setup.bash
+++ .devcontainer/setup.bash
@@ -2,7 +2,7 @@
 # abort on error, undefined vars, and failed pipelines
 set -euo pipefail
 
-rm -f Dockerfile .gitpod.yml && rm -rf .github
+rm -f Dockerfile .gitpod.yml && rm -rf .github .devcontainer
 echo -e "# Snakemake-Tutorial\n\nYour gitpod workspace for the snakemake-tutorial has been initialized. Now you can start with the [basic tutorial](https://snakemake.readthedocs.io/en/stable/tutorial/basics.html)." > README.md
 echo "*" > .gitignore
 conda init
