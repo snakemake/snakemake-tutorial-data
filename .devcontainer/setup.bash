ae2391ef2c6606451d922c5127e0ae6571f8a718
Johannes Koester <johannes.koester@uni-due.de>
2025-07-09 11:39:06 +0200

    fix

--- .devcontainer/setup.bash
+++ .devcontainer/setup.bash
@@ -1,6 +1,6 @@
 rm -r Dockerfile .gitpod.yml .github
 echo -e "# Snakemake-Tutorial\n\nYour gitpod workspace for the snakemake-tutorial has been initialized. Now you can start with the [basic tutorial](https://snakemake.readthedocs.io/en/stable/tutorial/basics.html)." > README.md
-echo \"*\" > .gitignore
+echo "*" > .gitignore
 conda init
 source ~/.bashrc
-echo 'conda activate snakemake-tutorial' >> ~/.bashrc"
+echo 'conda activate snakemake-tutorial' >> ~/.bashrc
